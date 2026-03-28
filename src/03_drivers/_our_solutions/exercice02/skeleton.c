7754ebc4f6c60fb72ca01920384a0f3f70d5858f
Carolina Inácio Pereira <carolina.inaciope@hes-so.ch>
2026-03-28 16:23:43 +0100

    chore: upload our solutions for the drivers exercises until now

---
+++ src/03_drivers/_our_solutions/exercice02/skeleton.c
@@ -0,0 +1,148 @@
+/* skeleton.c */
+#include <linux/cdev.h>        /* needed for char device driver */
+#include <linux/fs.h>          /* needed for device drivers */
+#include <linux/init.h>        /* needed for macros */
+#include <linux/kernel.h>      /* needed for debugging */
+#include <linux/module.h>      /* needed by all modules */
+#include <linux/moduleparam.h> /* needed for module parameters */
+#include <linux/uaccess.h>     /* needed to copy data to/from user */
+
+#define BUFFER_SZ 10000
+
+// This is the global variable that stocks the data written
+static char s_buffer[BUFFER_SZ];
+// This is the device number
+static dev_t skeleton_dev;
+// This is the device structure
+static struct cdev skeleton_cdev;
+
+// This function is called when the device file is opened
+static int skeleton_open(struct inode* i, struct file* f)
+{
+	// Print the major and minor number of the device being opened
+    pr_info("skeleton : open operation... major:%d, minor:%d\n",
+            imajor(i),
+            iminor(i));
+
+	// Print the access mode of the file being opened
+
+    if ((f->f_flags & (O_APPEND)) != 0) {
+        pr_info("skeleton : opened for appending...\n");
+    }
+
+    if ((f->f_mode & (FMODE_READ | FMODE_WRITE)) != 0) {
+        pr_info("skeleton : opened for reading & writing...\n");
+    } else if ((f->f_mode & FMODE_READ) != 0) {
+        pr_info("skeleton : opened for reading...\n");
+    } else if ((f->f_mode & FMODE_WRITE) != 0) {
+        pr_info("skeleton : opened for writing...\n");
+    }
+
+    return 0;
+}
+
+// This function is called when the device file is closed
+static int skeleton_release(struct inode* i, struct file* f)
+{
+    pr_info("skeleton: release operation...\n");
+
+    return 0;
+}
+
+// This function is called when the device file is read
+static ssize_t skeleton_read(struct file* f,
+                             char __user* buf,
+                             size_t count,
+                             loff_t* off)
+{
+    // compute remaining bytes to copy, update count and pointers
+	// - remaining bytes to copy is the total buffer size minus the offset
+    ssize_t remaining = BUFFER_SZ - (ssize_t)(*off);
+	// - ptr is the pointer to the buffer at the offset
+    char* ptr         = s_buffer + *off;
+
+	// - if the count of bytes to read is greater than the remaining bytes, update the count to the remaining bytes
+    if (count > remaining) count = remaining;
+    *off += count;
+
+    // copy required number of bytes 
+	// (from the internal buffer to the user buffer)
+    if (copy_to_user(buf, ptr, count) != 0) count = -EFAULT;
+
+    pr_info("skeleton: read operation... read=%ld\n", count);
+
+    return count;
+}
+
+// This function is called when the device file is written
+static ssize_t skeleton_write(struct file* f,
+                              const char __user* buf,
+                              size_t count,
+                              loff_t* off)
+{
+    // compute remaining space in buffer and update pointers
+    ssize_t remaining = BUFFER_SZ - (ssize_t)(*off);
+
+    pr_info("skeleton: at%ld\n", (unsigned long)(*off));
+
+    // check if still remaining space to store additional bytes
+	// - if the count of bytes to write is greater than the remaining space, update the count to an error code
+    if (count >= remaining) count = -EIO;
+
+    // store additional bytes into internal buffer
+    if (count > 0) {
+        char* ptr = s_buffer + *off;
+        *off += count;
+        ptr[count] = 0;  // make sure string is null terminated
+
+		// if the copy from user to kernel buffer fails, update the count to an error code
+        if (copy_from_user(ptr, buf, count)) count = -EFAULT;
+    }
+
+    pr_info("skeleton: write operation... written=%ld\n", count);
+
+    return count;
+}
+
+// This structure defines the file operations that the device supports
+static struct file_operations skeleton_fops = {
+    .owner   = THIS_MODULE,
+    .open    = skeleton_open,
+    .read    = skeleton_read,
+    .write   = skeleton_write,
+    .release = skeleton_release,
+};
+
+// This function is called when the module is loaded
+static int __init skeleton_init(void)
+{
+	// Allocate a device number for the character device
+    int status = alloc_chrdev_region(&skeleton_dev, 0, 1, "mymodule");
+
+	// If the allocation is successful, initialize the character device and add it to the system
+    if (status == 0) {
+        cdev_init(&skeleton_cdev, &skeleton_fops);
+        skeleton_cdev.owner = THIS_MODULE;
+        status              = cdev_add(&skeleton_cdev, skeleton_dev, 1);
+    }
+
+    pr_info("Linux module skeleton loaded\n");
+    return 0;
+}
+
+// This function is called when the module is unloaded
+static void __exit skeleton_exit(void)
+{
+	// Remove the character device from the system and unregister the device number
+    cdev_del(&skeleton_cdev);
+    unregister_chrdev_region(skeleton_dev, 1);
+
+    pr_info("Linux module skeleton unloaded\n");
+}
+
+module_init(skeleton_init);
+module_exit(skeleton_exit);
+
+MODULE_AUTHOR("Daniel Gachet <daniel.gachet@hefr.ch>");
+MODULE_DESCRIPTION("Module skeleton");
+MODULE_LICENSE("GPL");
