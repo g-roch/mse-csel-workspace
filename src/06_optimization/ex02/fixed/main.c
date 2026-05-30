752fe11239675bc4fc120bf7ba32d2bfbb965365
Carolina Inácio Pereira <carolina.inaciope@hes-so.ch>
2026-05-30 11:20:24 +0000

    chore(06_optimization): add work from Carolina

---
+++ src/06_optimization/ex02/fixed/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 65536
+
+// Comparison function for qsort
+static int compare (const void* a, const void* b)
+{
+    return *(short*)a - *(short*)b;
+}
+
+int main()
+{
+ 	// generate data
+	short data[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		data[i] = rand() % 512;
+	}
+
+	// Sort the data to improve branch prediction
+	qsort(data, SIZE, sizeof(data[0]), compare);
+
+	long long sum = 0;
+	for (int j = 0; j < 10000; j++) {
+		for (int i = 0; i < SIZE; i++) {
+			if (data[i] >= 256) {
+				sum += data[i];
+			}
+		}
+	}
+	printf ("sum=%lld\n", sum);
+}
