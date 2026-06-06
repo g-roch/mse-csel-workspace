9394a9e9177823f1acde4067fc3541a2f5f68a25
Carolina Inácio Pereira <carolina.inaciope@hes-so.ch>
2026-06-06 12:37:14 +0000

    fix(07_miniproj): fix high CPU usage by the daemon

--- src/07_miniproj/button_control/config.h
+++ src/07_miniproj/button_control/config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define SET_FREQ_PATH "/tmp/set_freq" // TODO: add real path in class later
-#define GET_FREQ_PATH "/tmp/set_freq" // TODO: add real path in class later (same as set_freq bc we don't have the sysmod yet)
-#define AUTO_FREQ_PATH "/tmp/auto_freq" // TODO: add real path in class later
+#define SET_FREQ_PATH "/sys/kernel/led/set_freq"
+#define GET_FREQ_PATH "/sys/kernel/led/get_freq"
+#define AUTO_FREQ_PATH "/sys/kernel/led/auto_freq"
 #define GET_TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
 
 #define PID_FILE "/run/led_daemon.pid"
